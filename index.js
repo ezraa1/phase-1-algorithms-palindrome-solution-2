79e9fb2b1d32719868d17e6c09fc53e78b47026a
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-06-19 11:26:18 +0300

    commit tree c6260313e2f819c046fa99799b29a60edb46514b

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    const j = word.length - 1 - i;
+    if (word[ i ] !== word[ j ]) {
+      
+      return false;
+    }
+    return true;
+  }
 }
+function isPalindrome(word) {
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
 
+return true;
+}
 /* 
   Add your pseudocode here
+ - iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+- if the letters don't match, return false
+- if we reach the middle, and all the letters match, return true
+  
 */
 
 /*
   Add written explanation of your solution here
+  - iterate from the beginning of the string to the middle of the string
+  - compare the letter we're iterating over to the corresponding letter at the end of the string
+  - if the letters don't match, return false
+  - if we reach the middle, and all the letters match, return true
 */
 
 // You can run `node index.js` to view these console logs
@@ -17,6 +43,26 @@ if (require.main === module) {
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
